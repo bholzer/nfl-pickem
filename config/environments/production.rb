78ff54fac114bc6b55b53f6bb39b54582f1173e9
Brennan Holzer <brennan@shipogre.com>
2025-10-29 10:28:07 -0500

    Set default host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,8 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+  config.action_mailer.default_url_options = { host: ENV["RAILS_HOST"] || "pickem.unboundops.com" }
+  config.action_controller.default_url_options = { host: ENV["RAILS_HOST"] || "pickem.unboundops.com" }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
   # config.action_mailer.smtp_settings = {
